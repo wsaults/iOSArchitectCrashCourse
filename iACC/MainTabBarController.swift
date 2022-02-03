d174ea068768a6fa78e8ffdd175ba6a4df49d607
wsaults <will@saults.io>
2022-02-03 16:21:17 -0600

    Extract CardAPI logic into composition adapter

--- iACC/MainTabBarController.swift
+++ iACC/MainTabBarController.swift
@@ -87,6 +87,15 @@ class MainTabBarController: UITabBarController {
 	private func makeCardsList() -> ListViewController {
 		let vc = ListViewController()
 		vc.fromCardsScreen = true
+        vc.shouldRetry = false
+        vc.title = "Cards"
+        vc.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: vc, action: #selector(addCard))
+
+        vc.service = CardAPIItemsServiceAdapter(
+            api: CardAPI.shared,
+            select: { [weak vc] item in
+                vc?.select(card: item)
+            })
 		return vc
 	}
 }
@@ -104,6 +113,25 @@ struct FriendsAPIItemsServiceAdapter: ItemsService {
 
                     return items.map { item in
                         ItemViewModel(friend: item) {
+                            select(item)
+                        }
+                    }
+                })
+            }
+        }
+    }
+}
+
+struct CardAPIItemsServiceAdapter: ItemsService {
+    let api: CardAPI
+    let select: (Card) -> Void
+
+    func loadItems(completion: @escaping (Result<[ItemViewModel], Error>) -> Void) {
+        api.loadCards { result in
+            DispatchQueue.mainAsyncIfNeeded {
+                completion(result.map { items in
+                    items.map { item in
+                        ItemViewModel(card: item) {
                             select(item)
                         }
                     }
